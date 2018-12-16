eccb7854592922d9531ff8b209241599ed784ac2
Jitin <paul.jitin@gmail.com>
2018-12-15 23:07:37 -0500

    Delete cache files

--- tic-tac-toe/mysite/settings.py
+++ tic-tac-toe/mysite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'about.apps.AboutConfig',
     'tictactoe.apps.TictactoeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- tic-tac-toe/mysite/urls.py
+++ tic-tac-toe/mysite/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^about/', include('about.urls')),
-    url(r'^tictactoe/', include('tictactoe.urls')),
+    url(r'', include('tictactoe.urls')),
     url(r'^admin/', admin.site.urls),
 ]
